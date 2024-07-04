f5a9467b5bf876f8b1e4f64553d0e5cd391e87af
User01 <rigbykai1@gmail.com>
2024-07-04 00:23:45 -0600

    Server implementation

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./App')
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
