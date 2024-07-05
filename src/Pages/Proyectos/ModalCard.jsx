59814f626e502c775c35a2d0930189699eab388c
User01 <rigbykai1@gmail.com>
2024-07-05 17:24:07 -0600

    Modal images bug

--- src/Pages/Proyectos/ModalCard.jsx
+++ src/Pages/Proyectos/ModalCard.jsx
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
 
 const ModalCard = (props) => {
     const { bgImage1, bgImage2, id } = props;
-
+    const [currentImage, setCurrentImage] = useState(bgImage1);
 
     const idCatcher = (str) => {
         const sections = str.split('/');
@@ -13,23 +14,27 @@ const ModalCard = (props) => {
         window.open(img, '_blank');
     };
 
+    const handleNextImage = () => {
+        setCurrentImage(bgImage2);
+    };
+
+    const handlePrevImage = () => {
+        setCurrentImage(bgImage1);
+    };
+
     return (
         <dialog id={id} className="modal backdrop-blur-lg">
             <div className="modal-box w-11/12 max-w-2xl bg-black/50 p-2">
                 <div className="carousel rounded-xl">
-                    <a id={idCatcher(bgImage1)} className="carousel-item w-fit" onClick={() => openImage(bgImage1)}>
-                        <img src={bgImage1} className="cursor-pointer" />
+                    <a id={idCatcher(currentImage)} className="carousel-item w-fit" onClick={() => openImage(currentImage)}>
+                        <img src={currentImage} className="cursor-pointer" />
                     </a>
-                    <a id={idCatcher(bgImage2)} className="carousel-item w-fit" onClick={() => openImage(bgImage2)}>
-                        <img src={bgImage2} className="cursor-pointer" />
-                    </a>
-
                 </div>
                 <div className="modal-action justify-center">
                     <form method="dialog" className='space-x-10'>
-                        <a href={`#${idCatcher(bgImage1)}`} className="btn btn-outline btn-sm hover:scale-110"><FaAngleDoubleLeft /></a>
+                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={handlePrevImage}><FaAngleDoubleLeft /></button>
                         <button className="btn btn-outline btn-accent btn-md hover:scale-110" >Cerrar</button>
-                        <a href={`#${idCatcher(bgImage2)}`} className="btn btn-outline btn-sm hover:scale-110"><FaAngleDoubleRight /></a>
+                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={handleNextImage}><FaAngleDoubleRight /></button>
                     </form>
                 </div>
             </div>
@@ -37,4 +42,4 @@ const ModalCard = (props) => {
     )
 }
 
-export default ModalCard
+export default ModalCard;
