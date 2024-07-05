3da36787763e31c267855e9bdc107340cbcbbe56
User01 <rigbykai1@gmail.com>
2024-07-05 16:09:37 -0600

    Trying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  base: "https://rigbykai1.github.io/Portfolio/",
+
 })
