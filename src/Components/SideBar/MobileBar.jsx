f3078b71016a9ceed1e43aa822aebe52f28f65d9
User01 <rigbykai1@gmail.com>
2024-07-05 16:58:36 -0600

    MobileBar update

--- src/Components/SideBar/MobileBar.jsx
+++ src/Components/SideBar/MobileBar.jsx
@@ -1,28 +1,29 @@
 import { SiAboutdotme } from "react-icons/si";
 import { MdContactMail } from "react-icons/md";
 import { GrProjects } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 const MobileBar = () => {
     return (
         <div className="fixed bottom-0 left-0 right-0 bg-white/10 py-2 backdrop-blur-lg z-10 md:hidden">
             <div className="flex justify-around">
-                <button><a href="/Contacto">
+                <button><Link to="/Contacto">
                     <MdContactMail className="w-26" />
-                </a></button>
-                <button><a href="/Proyectos">
+                </Link></button>
+                <button><Link to="/Proyectos">
                     <GrProjects className="w-26" />
-                </a></button>
-                <button><a href="/SobreMi">
+                </Link></button>
+                <button><Link to="/SobreMi">
                     <SiAboutdotme className="w-26" />
-                </a></button>
+                </Link></button>
                 <button>
-                    <a href="/Home">
+                    <Link to="/Home">
                         <div className="avatar">
                             <div className="w-10 rounded-full">
                                 <img src="https://avatars.githubusercontent.com/u/129445798?v=4" />
                             </div>
                         </div>
-                    </a>
+                    </Link>
                 </button>
             </div>
         </div >
