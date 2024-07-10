0bc1ebe9c3954ea19758e59eb0cdd0cf93d76430
User01 <rigbykai1@gmail.com>
2024-07-09 22:33:23 -0600

    new domain

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,4 +1,3 @@
-// AppLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SideBar from "../Components/SideBar/SideBar"
--- src/Pages/Proyectos/ModalCard.jsx
+++ src/Pages/Proyectos/ModalCard.jsx
@@ -3,7 +3,8 @@ import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
 
 const ModalCard = (props) => {
     const { bgImage1, bgImage2, id } = props;
-    const [currentImage, setCurrentImage] = useState(bgImage1);
+    const images = [bgImage1, bgImage2];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const idCatcher = (str) => {
         const sections = str.split('/');
@@ -14,27 +15,26 @@ const ModalCard = (props) => {
         window.open(img, '_blank');
     };
 
-    const handleNextImage = () => {
-        setCurrentImage(bgImage2);
-    };
-
-    const handlePrevImage = () => {
-        setCurrentImage(bgImage1);
+    const handleChangeImage = (direction) => {
+        setCurrentImageIndex((prevIndex) => {
+            const newIndex = (prevIndex + direction + images.length) % images.length;
+            return newIndex;
+        });
     };
 
     return (
         <dialog id={id} className="modal backdrop-blur-lg">
             <div className="modal-box w-11/12 max-w-2xl bg-black/50 p-2">
                 <div className="carousel rounded-xl">
-                    <a id={idCatcher(currentImage)} className="carousel-item w-fit" onClick={() => openImage(currentImage)}>
-                        <img src={currentImage} className="cursor-pointer" />
+                    <a id={idCatcher(images[currentImageIndex])} className="carousel-item w-fit" onClick={() => openImage(images[currentImageIndex])}>
+                        <img src={images[currentImageIndex]} className="cursor-pointer" />
                     </a>
                 </div>
                 <div className="modal-action justify-center">
                     <form method="dialog" className='space-x-10'>
-                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={handlePrevImage}><FaAngleDoubleLeft /></button>
+                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={() => handleChangeImage(-1)}><FaAngleDoubleLeft /></button>
                         <button className="btn btn-outline btn-accent btn-md hover:scale-110" >Cerrar</button>
-                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={handleNextImage}><FaAngleDoubleRight /></button>
+                        <button type="button" className="btn btn-outline btn-sm hover:scale-110" onClick={() => handleChangeImage(1)}><FaAngleDoubleRight /></button>
                     </form>
                 </div>
             </div>
