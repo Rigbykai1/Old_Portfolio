0c17079ff4b6d5b197cb6ee58878436fbac7f748
User01 <rigbykai1@gmail.com>
2024-07-09 22:34:11 -0600

    new domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rigbykai1.github.io/Portfolio/",
+  base: "https://alliances.life/",
 })
