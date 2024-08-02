94add8a7a2118633a08ff1fba51c760183cb0087
User01 <rigbykai1@gmail.com>
2024-08-02 15:14:04 -0600

    finish touches

--- src/Pages/Proyectos/PasswordGen/PasswordGen.jsx
+++ src/Pages/Proyectos/PasswordGen/PasswordGen.jsx
@@ -1,45 +1,41 @@
 import ProyectoPage from "../ProyectoTemplate/ProyectoPage";
-import { FaReact, FaNodeJs } from "react-icons/fa";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { SiMongodb, SiExpress, SiReactrouter } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
+
 const PasswordGen = () => {
     const funcionalidades = {
         funcionalidad1: {
-            titulo: 'Login en Desarrollo',
-            descripcion: 'Actualmente, se está trabajando en una funcionalidad de login para mejorar la seguridad y personalización de la experiencia del usuario.'
+            titulo: 'Generación de Contraseña Segura',
+            descripcion: 'Genera una contraseña segura combinando letras, números y caracteres especiales, asegurando una alta entropía.'
         },
         funcionalidad2: {
-            titulo: 'Agregar Notas con Marcador de Importancia',
-            descripcion: 'Permite a los usuarios agregar notas con un marcador de importancia, facilitando la organización y priorización de la información.'
+            titulo: 'Selección de Longitud de Contraseña',
+            descripcion: 'Permite al usuario especificar la longitud deseada de la contraseña, ofreciendo flexibilidad según las necesidades de seguridad.'
         },
         funcionalidad3: {
-            titulo: 'Filtro de Notas',
-            descripcion: 'Incluye un filtro que permite a los usuarios clasificar rápidamente las notas importantes y las que no lo son, optimizando la gestión de sus tareas.'
+            titulo: 'Validación de Entrada de Usuario',
+            descripcion: 'Valida que la entrada del usuario para la longitud de la contraseña sea un número entero, manejando errores de entrada no numérica.'
         },
         funcionalidad4: {
-            titulo: 'Integración con MongoDB',
-            descripcion: 'La aplicación está conectada a una base de datos MongoDB, asegurando un almacenamiento eficiente y seguro de las notas y otra información relevante.'
+            titulo: 'Copia Automática al Portapapeles',
+            descripcion: 'Copia automáticamente la contraseña generada al portapapeles del sistema, facilitando su uso inmediato en otras aplicaciones.'
         },
+        funcionalidad5: {
+            titulo: 'Interfaz de Línea de Comandos',
+            descripcion: 'Utiliza una interfaz de línea de comandos simple e interactiva para guiar al usuario a través del proceso de generación de la contraseña.'
+        }
     }
 
     const proyectoInfo = {
         titulo: 'Password Gen',
         imageUrl: 'https://raw.githubusercontent.com/Rigbykai1/Portfolio/master/src/Sources/PasswordGen/shot1.jpg',
-        sourceLink: '',
-        gitHubLink: 'https://github.com/Rigbykai1/NotasTailWind',
+        sourceLink: 'https://raw.githubusercontent.com/Rigbykai1/Portfolio/master/src/Sources/Downloads/PasswordGen.zip',
+        gitHubLink: 'https://github.com/Rigbykai1/PasswordGen',
         youtubeLink: '',
-        primerParrafo: 'Esta aplicación, desarrollada en React como parte del curso Full Stack Open, ha sido adaptada con estilos de Tailwind y mejorada con funcionalidades adicionales no incluidas en el proyecto original.',
-        segundoParrafo: 'Fue un excelente proyecto donde aprendí a manejar código con bases de datos, específicamente usando MongoDB. También adquirí conocimientos valiosos sobre cómo realizar peticiones a la base de datos, habilidades esenciales para resolver problemas y mejorar la vida cotidiana.',
+        primerParrafo: 'Quise realizar este script para crear contraseñas aleatorias con determinada longitud para la creación de mis contraseñas seguras.',
+        segundoParrafo: 'Utilicé librerías que ya tiene intregradas Python para facilitar el desarrollo de la misma, se utilizó la librería Pyperclip que si es una librería fuera de Python, pero que cumple la función que necesitamos, que es copiar la contraseña al portapapeles una vez creada.',
     }
     const tecnologias = {
-        tecnologia1: { titulo: 'React', icono: <FaReact className="size-8" />, color: "hover:text-blue-400", href: "https://react.dev/" },
-        tecnologia2: { titulo: 'Tailwind CSS', icono: <RiTailwindCssFill className="size-8" />, color: "hover:text-cyan-400", href: "https://tailwindcss.com/" },
-        tecnologia3: { titulo: 'MongoDB', icono: <SiMongodb className="size-8" />, color: "hover:text-green-400", href: "https://www.mongodb.com/" },
-        tecnologia4: { titulo: 'Node.js', icono: <FaNodeJs className="size-8" />, color: "hover:text-green-700", href: "https://nodejs.org/en" },
-        tecnologia5: { titulo: 'Express.js', icono: <SiExpress className="size-8" />, color: "hover:text-blue-700", href: "https://expressjs.com/" },
-        tecnologia6: {
-            titulo: 'React Router', icono: <SiReactrouter className="size-8" />, color: "hover:text-red-400", href: "https://reactrouter.com/en/main"
-        },
+        tecnologia1: { titulo: 'Python', icono: <FaPython className="size-8" />, color: "hover:text-yellow-400", href: "https://react.dev/" },
     }
     return (
         <>
