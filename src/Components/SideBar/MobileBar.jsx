41ac3a65ff1555dc2a1444a19d1e0fa2168da7d1
User01 <rigbykai1@gmail.com>
2024-08-02 20:39:50 -0600

    Mobile bar paddin

--- src/Components/SideBar/MobileBar.jsx
+++ src/Components/SideBar/MobileBar.jsx
@@ -4,23 +4,20 @@ import { Link } from "react-router-dom";
 
 const MobileBar = () => {
     return (
-        <div className="fixed bottom-0 left-0 right-0 bg-white/10 py-2 backdrop-blur-lg z-10 md:hidden">
+        <div className="fixed bottom-0 left-0 right-0 bg-white/10 py-5 backdrop-blur-lg z-10 md:hidden">
             <div className="flex justify-around text-white">
                 <button><Link to="/Contacto" className="flex flex-col items-center">
                     <MdContactMail className="size-6" />
                     <span className="text-center">Contacto</span>
                 </Link>
                 </button>
-
                 <button><Link to="/Proyectos" className="flex flex-col items-center">
                     <GrProjects className="size-6" />
                     <span className="text-center">Proyectos</span>
-
                 </Link></button>
                 <button><Link to="/Servicios" className="flex flex-col items-center">
                     <MdHomeRepairService className="size-6" />
                     <span className="text-center">Servicios</span>
-
                 </Link></button>
                 <button>
                     <Link to="/" className="flex flex-col items-center">
